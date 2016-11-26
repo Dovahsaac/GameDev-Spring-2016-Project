8b9cac32776eda9992ca3153ec997a41f3e4d09e
Rodney Knight <rk733@uowmail.edu.au>
2016-11-27 04:25:49 +1100

    added script for game over functionality

---
+++ Assets/gameOver.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class gameOver : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+        gameObject.SetActive(false);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKeyDown("space"))
+            SceneManager.LoadScene("Main Menu");
+
+    }
+
+    public void show()
+    {
+        gameObject.SetActive(true);
+    }
+}
