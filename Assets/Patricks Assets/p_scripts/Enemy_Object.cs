9ecf686d9c3fd5dab655c43007caf55535f9a609
Pat8u <Young@Patricks-MacBook-Pro.local>
2016-11-26 21:12:19 +1100

    Lives system and enemies

---
+++ Assets/Patricks Assets/p_scripts/Enemy_Object.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_Object : MonoBehaviour {
+	public GameObject player;
+	private Vector3 position;
+	public float xvar;
+	public GameObject bullet;
+	private GameObject newbullet;
+	private bool hasfired;
+	private Vector3 lagpos;
+	// Use this for initialization
+	void Start () {
+		hasfired = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		position = player.transform.position;
+		position.x = position.x - xvar;
+		position.y = position.y + 5;
+		position.z = position.z + 5;
+		transform.position = Vector3.MoveTowards (transform.position, position, 2f * Time.deltaTime);
+		if (hasfired == false) {
+			StartCoroutine (shoottime ());
+			hasfired = true;
+			lagpos = player.transform.position;
+			shoot ();
+		}
+
+		newbullet.transform.position = Vector3.MoveTowards(newbullet.transform.position,lagpos,10f*Time.deltaTime);
+		if (newbullet.transform.position == lagpos && hasfired == true) {
+			Destroy (newbullet);
+			hasfired = false;
+		}
+	}
+
+	void shoot(){
+		newbullet = Instantiate (bullet);
+		newbullet.GetComponent<MeshRenderer>().enabled = true;
+
+	
+	
+	
+	}
+
+	IEnumerator shoottime(){
+		yield return new WaitForSeconds (20);
+
+
+
+	}
+
+}
---
+++ Assets/Patricks Assets/p_scripts/Enemy_spawner.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy_spawner : MonoBehaviour {
+	
+	private int amountofenemies;
+	public GameObject enemy;
+	private int xchang;
+	private GameObject newenemy;
+	// Use this for initialization
+	void Start () {
+		amountofenemies = 0;
+		xchang = 0;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (amountofenemies == 0) {
+			createenemy ();
+			amountofenemies++;
+		} 
+		else if (amountofenemies < 3) {
+			StartCoroutine (spawn ());
+			amountofenemies++;
+			
+		}
+	}
+
+	IEnumerator spawn () {
+		if (amountofenemies == 1) {
+			yield return new WaitForSeconds (10);
+			createenemy ();
+		}
+		if (amountofenemies == 2) {
+			yield return new WaitForSeconds (40);
+			createenemy ();
+		}
+	}
+
+
+	void createenemy(){
+		newenemy = Instantiate (enemy);
+		newenemy.GetComponent<Enemy_Object>().xvar = xchang;
+		newenemy.GetComponent<MeshRenderer> ().enabled = true;
+		newenemy.GetComponent<Enemy_Object> ().enabled = true;
+		xchang = xchang + 5;
+
+	}
+
+
+
+
+}
