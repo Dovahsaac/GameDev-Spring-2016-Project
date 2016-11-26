076cf672b04ebccc9b042c5e4aad2f26b1960160
Pat8u <Young@Patricks-MacBook-Pro.local>
2016-11-26 17:13:59 +1100

    Added more stuff
    Advanced controls , Collision , Respawning and the starts of the ship
    class

--- Assets/Patricks Assets/p_scripts/shipControl.cs
+++ Assets/Patricks Assets/p_scripts/shipControl.cs
@@ -6,11 +6,22 @@ public class shipControl : MonoBehaviour {
 	public Vector3 boundneg = new Vector3(-5,0,0);
 	public Vector3 boundpos = new Vector3(5,0,0);
 	public Vector3 disBehind;
+	public Vector3 xvar;
 	public Camera camera = new Camera ();
+	public GameObject respawner;
 	public GameObject pointer;
+	public Vector3 originalpos;
+	public GameObject respawntext;
+	private TextMesh newtex;
+	private MeshRenderer texrend;
 	// Use this for initialization
+	public Rigidbody rigid;
+	private Ship ship = new Ship ();
 	void Start () {
-		
+		originalpos = transform.position;
+		rigid = GetComponent<Rigidbody> ();
+		newtex = respawntext.GetComponent<TextMesh> ();
+		texrend = respawntext.GetComponent<MeshRenderer> ();
 	}
 	
 	// Update is called once per frame
@@ -36,19 +47,63 @@ public class shipControl : MonoBehaviour {
 		test.z = pointer.transform.position.z - Camera.main.transform.position.z;
 		test = Camera.main.ScreenToWorldPoint (test);
 		disBehind.z = transform.position.z - Camera.main.transform.position.z;
-		//disBehind.x = transform.position.x - Camera.main.transform.position.x;
+
 		disBehind = Camera.main.ScreenToWorldPoint (disBehind);
+		disBehind.y = test.y -1f;
 		pointer.transform.position = /*Vector3.MoveTowards (pointer.transform.position,*/ test /*, force * Time.deltaTime)*/;
 
 
 
 		transform.position = Vector3.MoveTowards (transform.position, disBehind, force * Time.deltaTime);
 
-		Debug.Log (disBehind);
+	
+
+
 
 	}
+	void OnCollisionEnter(Collision col){
 
 
+
+		if (col.gameObject.tag == "Cube") {
+
+			Debug.Log ("Works");
+			GetComponent<MeshRenderer> ().enabled = false; 
+			GetComponent<BoxCollider> ().enabled = false;
+			transform.position = originalpos;
+			Debug.Log (originalpos);
+			StartCoroutine (respawn ());
+		} else {
+		
+			Debug.Log ("Doesn't");
+
+		}
+
+	
+	
+	}
+
+	IEnumerator respawn(){
+		texrend.enabled = true;
+		newtex.text = "Respawning in 5";
+		yield return new WaitForSeconds (1);
+		newtex.text = "Respawning in 4";
+		yield return new WaitForSeconds (1);
+		newtex.text = "Respawning in 3";
+		yield return new WaitForSeconds (1);
+		newtex.text = "Respawning in 2";
+		yield return new WaitForSeconds (1);
+		newtex.text = "Respawning in 1";
+		yield return new WaitForSeconds (1);
+		newtex.text = "Respawned";
+		GetComponent<MeshRenderer> ().enabled = true;
+
+		yield return new WaitForSeconds (2);
+		texrend.enabled = false;
+		GetComponent<BoxCollider> ().enabled = true;
+	
+	
+	}
 
 
 }
